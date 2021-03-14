ab526a8cd05b5c30601ab61cced1cf3d9e56465a
josemasf <josemasf@gmail.com>
2021-03-14 12:05:28 +0100

    add label info styling

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,7 @@ export default Vue.extend({
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Spartan:wght@500;700&display=swap');
-body{
+*{
   font-family: 'Spartan', sans-serif;
 }
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,21 +1,23 @@
 <template>
 <div class=" ">
-  <v-card outlined class="job-info mb-5" v-for="job in jobInfo" :key="job.id">
+  <v-card outlined class="job-info mb-5 pa-3" :class="job.featured ? 'featured-job': ''" v-for="job in jobInfo" :key="job.id">
   <v-list-item three-line >
     <v-list-item-avatar size="80">
       <v-img :src="logoImage(job)"></v-img>
     </v-list-item-avatar>
     <v-list-item-content>
-      <div class="overline ">
+      <v-row>
+        <v-col :cols=6>
+          <div class="primary--text job-company ">
         {{job.company}}
         <v-chip
-          x-small color="primary white--text ma-2" v-show="job.new"
+          x-small color="primary white--text ma-2 chip" v-show="job.new"
         >NEW!</v-chip>
         <v-chip
-          x-small color="black white--text" v-show="job.featured"
+          x-small color="black white--text text-uppercase chip" v-show="job.featured"
         >Featured</v-chip>
       </div>
-      <v-list-item-title class="mb-1">
+      <v-list-item-title class="mb-1 job-position">
         {{job.position}}
       </v-list-item-title>
       <v-list-item-subtitle>
@@ -25,6 +27,17 @@
           <li>{{job.location}}</li>
         </ul>         
       </v-list-item-subtitle>
+        </v-col>
+        <v-col :cols=6>
+          <div >
+            <v-chip class="primary--text font-weight-bold ma-2" color="filterbg"
+      label
+      v-for="item in labelInfo(job)" :key="item">
+              {{item}}
+            </v-chip>
+          </div>
+        </v-col>
+      </v-row>      
     </v-list-item-content>            
   </v-list-item>            
 </v-card>
@@ -47,6 +60,21 @@ export default class Card extends Vue{
 
       return require(`../assets/${fileName}`) // the module request
     }
+
+    labelInfo(jobInfo: Job) {      
+      const info: string[] = [];
+
+      info.push(jobInfo.role);
+      info.push(jobInfo.level);
+      jobInfo.languages.forEach(element => {
+        info.push(element)
+      });
+      jobInfo.tools.forEach(element => {
+        info.push(element)
+      });
+
+      return info;
+    }
 }
 
 interface Job{
@@ -54,25 +82,44 @@ interface Job{
     }
 </script>
 <style lang="scss" scoped>
-.job-info{
+.job-info {
   box-shadow: 0 0 25px 0 hsla(180, 29%, 50%, 0.4);
 }
-.extra-data{  
-  padding-left: 0;  
-    li {
-    display:inline;  
-    
+
+.extra-data {
+  padding-left: 0;
+
+  li {
+    display: inline;
+
   }
-    li:after{
-      content: '.';
-      margin: 0 15px;
-      position: relative;
-      top: -5px;
-      font-size: 20px;
-    }
-    li:last-child:after{
-        content: '';
-      }
-    
+
+  li:after {
+    content: '.';
+    margin: 0 15px;
+    position: relative;
+    top: -5px;
+    font-size: 20px;
+  }
+
+  li:last-child:after {
+    content: '';
+  }
+
+}
+
+.featured-job{
+  border-left: hsl(180, 29%, 50%) 5px solid !important;
+}
+.job-position{
+  font-weight: 700;
+  font-size: 18px;  
+}
+.job-company{
+  font-weight: 700;
+  font-size: 13px;  
+}
+.chip{
+  padding-top: 2.5px;
 }
 </style>
