a5c61844e10bcc4c340ea7ff0c619eb852d12da7
josemasf <josemasf@gmail.com>
2021-03-14 11:25:33 +0100

    add extra info styling

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -9,17 +9,21 @@
       <div class="overline ">
         {{job.company}}
         <v-chip
-          color="blue white--text" v-show="job.new"
-        >NEW !</v-chip>
+          x-small color="primary white--text ma-2" v-show="job.new"
+        >NEW!</v-chip>
         <v-chip
-          color="black white--text" v-show="job.featured"
+          x-small color="black white--text" v-show="job.featured"
         >Featured</v-chip>
       </div>
       <v-list-item-title class="mb-1">
         {{job.position}}
       </v-list-item-title>
       <v-list-item-subtitle>
-        {{job.postedAt}} - {{job.contract}} - {{job.location}}
+        <ul class="extra-data">
+          <li>{{job.postedAt}}</li>
+          <li>{{job.contract}}</li>
+          <li>{{job.location}}</li>
+        </ul>         
       </v-list-item-subtitle>
     </v-list-item-content>            
   </v-list-item>            
@@ -52,5 +56,23 @@ interface Job{
 <style lang="scss" scoped>
 .job-info{
   box-shadow: 0 0 25px 0 hsla(180, 29%, 50%, 0.4);
+}
+.extra-data{  
+  padding-left: 0;  
+    li {
+    display:inline;  
+    
+  }
+    li:after{
+      content: '.';
+      margin: 0 15px;
+      position: relative;
+      top: -5px;
+      font-size: 20px;
+    }
+    li:last-child:after{
+        content: '';
+      }
+    
 }
 </style>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,4 +4,14 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: '#5BA4A4',
+            secondary: '#7B8E8E',
+            deletefilter: '#2C3A3A',
+            filterbg: '#EEF6F6'
+          },
+        },
+      },
 });
